89ca371ee059c6222d12e5fd9a303acf87cdc9dd
atordjmann <tordjmann.audrey@gmail.com>
2019-11-05 15:33:00 +0100

    architecture initiale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { OffersComponent } from './offers/offers.component';
+import { LoggingComponent } from './logging/logging.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FilterComponent } from './offers/filter/filter.component';
+import { OfferPreviewComponent } from './offers/offer-preview/offer-preview.component';
+import { OfferDetailComponent } from './offers/offer-detail/offer-detail.component';
+import { ModalLoginComponent } from './logging/modal-login/modal-login.component';
+import { RegisterFormComponent } from './logging/register-form/register-form.component';
+import { ProfileDetailComponent } from './profile/profile-detail/profile-detail.component';
+import { ApplicationComponent } from './profile/application/application.component';
+import { NotificationComponent } from './profile/notification/notification.component';
+import { OfferSquareComponent } from './profile/application/offer-square/offer-square.component';
+import { OfferCompanyComponent } from './profile/application/offer-company/offer-company.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    OffersComponent,
+    LoggingComponent,
+    ProfileComponent,
+    FilterComponent,
+    OfferPreviewComponent,
+    OfferDetailComponent,
+    ModalLoginComponent,
+    RegisterFormComponent,
+    ProfileDetailComponent,
+    ApplicationComponent,
+    NotificationComponent,
+    OfferSquareComponent,
+    OfferCompanyComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/profile/application/offer-square/offer-square.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-square',
+  templateUrl: './offer-square.component.html',
+  styleUrls: ['./offer-square.component.sass']
+})
+export class OfferSquareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
