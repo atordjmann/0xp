62c3060059de7c85581e700b8ff57adbefbcb318
Paul TONDEREAU <paul.tondereau@soprasteria.com>
2019-11-26 17:13:11 +0100

    Test remove sass

--- src/app/profile/application/offer-square/offer-square.component.ts
+++ src/app/profile/application/offer-square/offer-square.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-offer-square',
   templateUrl: './offer-square.component.html',
-  styleUrls: ['./offer-square.component.sass']
+  styleUrls: ['./offer-square.component.scss']
 })
 export class OfferSquareComponent implements OnInit {
 
--- src/app/profile/profile-detail/profile-detail.component.ts
+++ src/app/profile/profile-detail/profile-detail.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile-detail',
   templateUrl: './profile-detail.component.html',
-  styleUrls: ['./profile-detail.component.sass']
+  styleUrls: ['./profile-detail.component.scss']
 })
 export class ProfileDetailComponent implements OnInit {
 
