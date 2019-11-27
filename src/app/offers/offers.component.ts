ee64f60e0a156d09069489aba9396d8cb3bdd2c8
Paul TONDEREAU <paul.tondereau@yahoo.fr>
2019-11-27 20:38:43 +0100

    Ajout des preview offre html sans style sauf %

--- src/app/offers/offers.component.ts
+++ src/app/offers/offers.component.ts
@@ -12,10 +12,25 @@ export class OffersComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.listOffers.push(new Offer(3, 'Développeur fullstack'));
-    this.listOffers.push(new Offer(4, 'Ingénieur R&D'));
-    this.listOffers.push(new Offer(5, 'Développeur back-end'));
-    this.listOffers.push(new Offer(6, 'Développeur javascript'));
+    let offer1 = new Offer()
+    offer1.title = "Junior Développeur Full Stack (H/F)"
+    offer1.srcImgCompany = "http://www.portify.fr/wp-content/uploads/2019/07/logo-sopra-steria.png"
+    offer1.company = "Sopra Steria"
+    offer1.date = new Date();
+    offer1.domain = "IT / Conseil"
+    offer1.location = "Marseille, France"
+    offer1.type = "Stage 4-6 Mois"
+    this.listOffers.push(offer1)
+
+    let offer2 = new Offer()
+    offer2.title = "Designer UI/UX Web (H/F)"
+    offer2.srcImgCompany = "https://upload.wikimedia.org/wikipedia/en/thumb/e/e4/ALTEN_logo.svg/1200px-ALTEN_logo.svg.png"
+    offer2.company = "Alten"
+    offer2.date = new Date();
+    offer2.domain = "Graphisme / Design"
+    offer2.location = "Paris, France"
+    offer2.type = "Alternance 3 ans"
+    this.listOffers.push(offer2)
   }
 
 }
