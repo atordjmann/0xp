9161cdda44d8705758d0f20e7f2c50110a49e9e8
Paul TONDEREAU <paul.tondereau@yahoo.fr>
2020-01-30 14:31:51 +0100

    Modif html add offer

--- front-angular/src/app/profile/application/offer-company/add-offer/add-offer.component.ts
+++ front-angular/src/app/profile/application/offer-company/add-offer/add-offer.component.ts
@@ -30,9 +30,6 @@ export const MY_FORMATS = {
   templateUrl: './add-offer.component.html',
   styleUrls: ['./add-offer.component.scss'],
   providers: [
-    // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
-    // application's root module. We provide it at the component level here, due to limitations of
-    // our example generation script.
     {
       provide: DateAdapter,
       useClass: MomentDateAdapter,
