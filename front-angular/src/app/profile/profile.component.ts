b0f7b4127b5e4f308480f064379b0153b0246521
atordjmann <tordjmann.audrey@gmail.com>
2020-01-31 17:46:00 +0100

    style

--- front-angular/src/app/profile/profile.component.ts
+++ front-angular/src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../logging/services';
 import { User } from 'src/models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -17,7 +18,8 @@ export class ProfileComponent implements OnInit {
   showNotifs: boolean;
   profile: any;
 
-  constructor(private authenticationService: AuthenticationService) {
+  constructor(private authenticationService: AuthenticationService,
+    private router: Router) {
     this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
    }
 
@@ -53,6 +55,11 @@ export class ProfileComponent implements OnInit {
     this.profile.photo = this.isStudent ?
     'https://i.pinimg.com/originals/aa/5a/27/aa5a270fc268cb82c66ef12e6def5a09.jpg' :
     'https://www.solutions-numeriques.com/wp-content/uploads/2016/10/sopra-steria1.png';
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
   }
 
 }
