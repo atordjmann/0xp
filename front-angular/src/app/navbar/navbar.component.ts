1f6139d2fab0686576f4f87ac1d1fd8b5f747eb3
Paul TONDEREAU <paul.tondereau@soprasteria.com>
2020-02-24 14:43:48 +0100

    Remove logo connexion si connecté sur profil ou login

--- front-angular/src/app/navbar/navbar.component.ts
+++ front-angular/src/app/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/models';
 import { AuthenticationService } from '../logging/services';
+import { GlobalService } from '../global.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -9,13 +11,20 @@ import { AuthenticationService } from '../logging/services';
 })
 export class NavbarComponent implements OnInit {
   currentUser: User;
-
-  constructor(private authenticationService: AuthenticationService
-    ) {
-      this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
-     }
+  isProfilOpen: Boolean;
+  isProfilOpenSubscription: Subscription;
+  constructor(private authenticationService: AuthenticationService, private globalService: GlobalService
+  ) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
 
   ngOnInit() {
+    this.isProfilOpen = this.globalService.isProfilOpen;
+    this.isProfilOpenSubscription = this.globalService.isProfilOpenSubject.subscribe(
+      (isProfilOpen: Boolean) => {
+        this.isProfilOpen = isProfilOpen;
+      }
+    );
   }
 
 }
--- front-angular/src/app/profile/profile.component.ts
+++ front-angular/src/app/profile/profile.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthenticationService } from '../logging/services';
 import { User } from 'src/models';
 import { Router } from '@angular/router';
+import { GlobalService } from '../global.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
 
   currentUser: any;
   isStudent: boolean;
@@ -17,7 +18,7 @@ export class ProfileComponent implements OnInit {
   showNotifs: boolean;
   profile: any;
 
-  constructor(private authenticationService: AuthenticationService,
+  constructor(private authenticationService: AuthenticationService, private globalService: GlobalService,
               private router: Router) {
     this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
   }
@@ -58,6 +59,11 @@ export class ProfileComponent implements OnInit {
     this.profile.photo = this.isStudent ?
       'https://i.pinimg.com/originals/aa/5a/27/aa5a270fc268cb82c66ef12e6def5a09.jpg' :
       this.currentUser.srcImage;
+    this.globalService.switchIsProfilOpen(true);
+  }
+
+  ngOnDestroy() {
+    this.globalService.switchIsProfilOpen(false);
   }
 
   logout() {
